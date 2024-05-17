ad12ef2a524726e98fad8be985e7a9edea4a790e
Javier Uncos <javiersrfc30@gmail.com>
2024-05-16 21:19:18 -0300

    quitamos estilos por defecto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Video Futbol</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
